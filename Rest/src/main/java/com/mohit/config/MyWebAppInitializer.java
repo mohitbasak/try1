f056dc6406a7b450bee2d7a3db55026015c5e74a
Mohit.Basak <Mohit.Basak@INC01014.Incture.local>
2019-06-13 14:55:18 +0530

    change commit for spring hibernate Skleton

---
+++ Rest/src/main/java/com/mohit/config/MyWebAppInitializer.java
@@ -0,0 +1,6 @@
+package com.mohit.config;
+
+public class MyWebAppInitializer {
+
+
+}
---
+++ Rest/src/main/java/com/mohit/config/SpringConfiguration.java
@@ -0,0 +1,12 @@
+package com.mohit.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "com.mohit")
+@EnableWebMvc
+public class SpringConfiguration {
+
+}
